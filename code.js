8720ded5b39c4fa1e7935e117231437e621815b1
Russell Mull <russell@c2klearning.com>
2014-02-12 15:50:32 -0800

    Initial revision - draws canvas

---
+++ code.js
@@ -0,0 +1,81 @@
+var doc = {
+    designedFor: "A customer",
+    designedBy: "ME!!!!",
+    date: "Today",
+    version: 7,
+    sections: {
+        keyPartners: ["wheeee!!!"],
+        keyActivities: [],
+        keyResources: [],
+        valuePropositions: [],
+        customerRelationships: [],
+        channels: [],
+        customerSegments: [],
+        costStructure: [],
+        revenueStreams: []
+    }
+};
+
+var tableLayout = [
+    [
+        { title: "Key Partners",
+          icon: "link",
+          key: "keyPartners",
+          rowspan: 2,
+          colspan: 2 },
+
+        { title: "Key Activities",
+          icon: "check",
+          key: "keyActivities",
+          rowspan: 1,
+          colspan: 2 },
+
+        { title: "Value Proposition",
+          icon: "gift",
+          key: "valuePropositions",
+          rowspan: 2,
+          colspan: 2 },
+
+        { title: "Customer Relationships",
+          icon: "heart",
+          key: "customerRelationships",
+          rowspan: 1,
+          colspan: 2 },
+
+        { title: "Customer Segments",
+          icon: "user",
+          key: "customerSegments",
+          rowspan: 2,
+          colspan: 2 }
+    ], [
+        { title: "Key Resources",
+          icon: "user",
+          key: "keyResources",
+          rowspan: 1,
+          colspan: 2 },
+
+        { title: "Channels",
+          icon: "user",
+          key: "channels",
+          rowspan: 1,
+          colspan: 2 }
+    ], [
+        { title: "Cost Structure",
+          icon: "usd",
+          key: "costStructure",
+          rowspan: 1,
+          colspan: 5 },
+
+        { title: "Revenue Streams",
+          icon: "usd",
+          key: "revenueStreams",
+          rowspan: 1,
+          colspan: 5 }
+    ]
+];
+
+
+function RootController($scope) {
+    $scope.doc = doc;
+    $scope.tableLayout = tableLayout;
+}
